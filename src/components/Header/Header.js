73db491f1a53f4605e2cf6ca287cb66eee46e0f1
Markus Rossio <argitavis@gmail.com>
2022-01-16 06:42:40 -0600

    Add hamburger icon to project, Add hamburger icon to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './Header.css';
+import hamburger from '../../assets/images/Hamburger.svg'
 
 export function Header () {
   return (
     <header className='header-container'>
-
+      <img src={hamburger} className='hamburger-img'/>
     </header>
   )
 }
