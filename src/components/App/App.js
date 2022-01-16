f7fadeb362d63453439331433daef8305a7f6010
Markus Rossio <argitavis@gmail.com>
2022-01-16 06:49:02 -0600

    Add matches container background color change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { fetchTournamentData, fetchMatchData, fetchTeamData, fetchGameData } from '../../ApiCalls/apiCalls';
 import './App.css';
 import { Header } from '../Header/Header';
+import { MatchesContainer } from '../MatchesContainer/MatchesContainer';
 
 function App() {
   useEffect(() => {
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MatchesContainer />
     </div>
   );
 }
---
+++ src/components/MatchesContainer/MatchesContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './MatchesContainer.css';
+
+export function MatchesContainer () {
+  return (
+    <section className='matches-container'>
+      
+    </section>
+  )
+}
