3f13ba77d2ae544c8f654f99922bbb9a5da36343
Markus Rossio <argitavis@gmail.com>
2022-01-18 00:28:10 -0600

    Add win/loss styling to match history card

--- src/components/MatchCard/MatchCard.js
+++ src/components/MatchCard/MatchCard.js
@@ -2,8 +2,10 @@ import React from "react";
 import './MatchCard.css';
 
 export function MatchCard ({ teams, isWin, id}) {
+  const winStyling = (isWin ? 'victory-background' : 'defeat-background') + ' match-container';
+
   return (
-    <article className='match-container'>
+    <article className={winStyling}>
       <div className='match-content-container'>
         <h1>{teams[0].opponent.acronym}</h1>
         <img className='team-logo' src={teams[0].opponent.image_url} alt='Team Logo' />
