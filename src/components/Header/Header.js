d8fdb077b4427b4584b0b43aab6cb2b6ee57a4fe
Markus Rossio <argitavis@gmail.com>
2022-01-18 11:00:24 -0600

    Remove uneeded img from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './Header.css';
-import historyImg from '../../assets/images/history.svg'
 import { Link } from "react-router-dom";
 
 export function Header ({favoriteTeamData}) {
